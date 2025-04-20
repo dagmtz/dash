20904e6fc0d4d1363262f38f9c6fd9f90c1dc345
dagmtz <dagmtzs@gmail.com>
2025-04-20 01:02:18 -0600

    create CMakeLists

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void lsh_loop(void){
+  return;
+}
+
+int main(int argc, char **argv){
+  // Load config files, if any.
+
+  // Run command loop.
+  lsh_loop();
+
+  // Perform any shutdown/cleanup.
+
+  return EXIT_SUCCESS;
+}
