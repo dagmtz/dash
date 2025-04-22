9b182de081f726fce6910be58f51bfd6c9de7bff
dagmtz <dagmtzs@gmail.com>
2025-04-21 21:42:46 -0600

    basic loop

--- src/main.c
+++ src/main.c
@@ -1,7 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void lsh_loop(void){
+char* dash_read_line(){
+  return NULL;
+}
+
+char** dash_split_line(char* line){
+  return NULL;
+}
+
+int dash_execute(char** args){
+  return 0;
+}
+
+void dash_loop(void){
+  char *line;
+  char **args;
+  int status;
+
+  do{
+    printf("> ");
+    line = dash_read_line();
+    args = dash_split_line(line);
+    status = dash_execute(args);
+
+    free(line);
+    free(args);
+  } while(status);
+
   return;
 }
 
@@ -9,9 +35,10 @@ int main(int argc, char **argv){
   // Load config files, if any.
 
   // Run command loop.
-  lsh_loop();
+  dash_loop();
 
   // Perform any shutdown/cleanup.
 
   return EXIT_SUCCESS;
 }
+
